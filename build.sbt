2008e16206545ac569907dd0c9cce6cde343e42d
Ellis Whitehead <ellis@ellisw.net>
2011-06-08 15:09:16 +0200

    sbt; added a Clean Token

--- build.sbt
+++ build.sbt
@@ -2,4 +2,7 @@ name := "roboliq"
 
 version := "1.0"
 
-scalaVersion := "2.9.0"
+scalaVersion := "2.9.0-1"
+
+
+seq(netbeans.NetbeansTasks.netbeansSettings:_*)
--- src/main/scala/roboliq/tokens/Token.scala
+++ src/main/scala/roboliq/tokens/Token.scala
@@ -3,8 +3,18 @@ package roboliq.tokens
 import roboliq.parts._
 
 
+object TipPart extends Enumeration {
+	val Inside, Outside = Value
+}
+
+object ContaminationDegree extends Enumeration {
+	val None, Minor, Medium, Major = Value
+}
+
 sealed class TipWellVolume(val tip: Tip, val well: Well, val nVolume: Double)
+sealed class TipContamination(val tip: Tip, val part: TipPart.Value, val contam: ContaminationDegree.Value, val nExtent: Double)
 
 sealed class Token
 case class Aspirate(twvs: Seq[TipWellVolume], rule: AspirateStrategy) extends Token
 case class Dispense(twvs: Seq[TipWellVolume], rule: DispenseStrategy) extends Token
+case class Clean(tcs: Seq[TipContamination])
