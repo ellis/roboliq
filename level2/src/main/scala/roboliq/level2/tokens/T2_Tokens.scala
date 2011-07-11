520ed89977a2d944f0fc99d3ebaa0951eb9c8ee5
Ellis Whitehead <ellis@ellisw.net>
2011-07-11 15:42:40 +0200

    mixing

--- level2/src/main/scala/roboliq/level2/tokens/T2_Tokens.scala
+++ level2/src/main/scala/roboliq/level2/tokens/T2_Tokens.scala
@@ -8,4 +8,4 @@ import roboliq.tokens._
 case class T2_Pipette(list: Seq[Tuple3[Well, Well, Double]]) extends T2_Token("pipette")
 case class T2_PipetteLiquid(srcs: SortedSet[Well], mapDestAndVolume: Map[Well, Double]) extends T2_Token("pipetteLiquid")
 case class T2_PipettePlate(src: Plate, dest: Plate, nVolume: Double) extends T2_Token("pipettePlate")
-case class T2_PipetteMix(wells: Seq[Well], nVolume: Double) extends T2_Token("pipetteMix")
+case class T2_PipetteMix(wells: SortedSet[Well], nVolume: Double) extends T2_Token("pipetteMix")
