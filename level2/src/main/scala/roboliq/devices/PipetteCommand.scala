53c456b0b7647ffa7d305b7a9f2af534b9d85427
Ellis Whitehead <ellis@ellisw.net>
2011-07-14 10:58:18 +0200

    thinking...

--- level2/src/main/scala/roboliq/devices/PipetteCommand.scala
+++ level2/src/main/scala/roboliq/devices/PipetteCommand.scala
@@ -2,10 +2,34 @@ package roboliq.devices
 
 import roboliq.parts._
 
+class KnowledgeBase {
+	val userObjects = new scala.collection.mutable.HashSet[Object]
+	
+	val liquidsNeedingLoc = new scala.collection.mutable.HashSet[Liquid]
+	val wellsNeedingLoc = new scala.collection.mutable.HashSet[Well]
+	val wellsNeedingLiquid = new scala.collection.mutable.HashSet[Well]
+	
+	private val m_mapLiquidToWells = new scala.collection.mutable.HashMap[Liquid, List[Well]]
+	
+	def getLiquidWells(liquid: Liquid): List[Well] = m_mapLiquidToWells.getOrElse(liquid, Nil)
+	
+	def addLiquid(liquid: Liquid) {
+		userObjects += liquid
+		if (!m_mapLiquidToWells.contains(liquid))
+			m_mapLiquidToWells(liquid) = Nil
+		if (m_mapLiquidToWells(liquid).isEmpty)
+			liquidsNeedingLoc += liquid
+	}
+	
+	def addWell(well: Well) {
+		userObject += well
+		well.holder
+	}
+}
 
 class TipSpec()
 class MixSpec(val nVolume: Double, val nCount: Int)
-class PipetteSpec(
+class PipetteItem(
 		val src: Object,
 		val dest: Object,
 		val nVolume: Double,
@@ -20,7 +44,24 @@ class PipetteArgs(
 )
 
 class PipetteCommand {
-	def pipette(args: PipetteArgs) {
-		
+	def pipette(kb: KnowledgeBase, items: Seq[PipetteItem]) {
+		val srcs = Set() ++ items.map(_.src)
+		if (srcs.size == 0)
+			return // TODO: add error handling
+		else {
+			val src0 = srcs.head
+			for (src <- srcs) {
+				if (src.isInstanceOf[Liquid]) {
+					val liquid = src.asInstanceOf[Liquid]
+					val wells = kb.getLiquidWells(liquid)
+					if (wells.isEmpty)
+						kb.liquidsNeedingLoc += liquid
+				}
+				else if (src.isInstanceOf[Well]) {
+					
+				}
+			}
+			val srcLiquids = 0
+		}
 	}
 }
