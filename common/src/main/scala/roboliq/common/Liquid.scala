6de6d43e9b62154f6f5d8bfb5b6fc3ebe818983d
Ellis Whitehead <ellis@ellisw.net>
2011-08-26 21:29:56 +0200

    thinking about tip handling

--- common/src/main/scala/roboliq/common/Liquid.scala
+++ common/src/main/scala/roboliq/common/Liquid.scala
@@ -1,5 +1,58 @@
 package roboliq.common
 
+object Contaminant extends Enumeration {
+	val Cell, Dna, Other = Value
+}
+
+object ContaminationLevel extends Enumeration {
+	val None, Minor, Major = Value
+}
+
+object TipReplacementAction extends Enumeration {
+	val None, Drop, Replace = Value
+}
+
+class ContaminationMap(map: Map[Contaminant.Value, ContaminationLevel.Value]) {
+	
+}
+
+class TipHandlingOverrides(
+	val replacement_? : Option[TipReplacementAction.Value],
+	val contaminantLevels: Map[Contaminant.Value, ContaminationLevel.Value]
+)
+
+class X() {
+	def chooseDispenseTipHandlingAction(overrides: TipHandlingOverrides, /*policy: PipettePolicy,*/ liquidInWell: Liquid2, contaminantsOnTip: Map[Contaminant.Value, ContaminationLevel.Value]): TipReplacementAction.Value = {
+		overrides.replacement_? match {
+			case Some(action) =>
+				action
+			case None =>
+				if (liquidInWell.bWaterFreeDispense) // FIXME: use 'policy' instead?
+					TipReplacementAction.None
+				else if (liquidInWell.bRequireDecontamBeforeAspirate)
+					TipReplacementAction.Replace
+				else if (contaminantsOnTip.keys.exists(liquidInWell.prohibitedTipContaminants.contains))
+					TipReplacementAction.Replace
+				else
+					TipReplacementAction.None
+		}
+	}
+	
+	def chooseDispenseWashAction(overrides: TipHandlingOverrides, /*policy: PipettePolicy,*/ liquidInWell: Liquid2, contaminantsOnTip: Map[Contaminant.Value, ContaminationLevel.Value]): TipReplacementAction.Value = {
+		
+	}
+}
+
+class Liquid2(
+	val sName: String,
+	val bWaterFreeDispense: Boolean,
+	val bRequireDecontamBeforeAspirate: Boolean,
+	/** Contaminants in this liquid */
+	val contaminantLevels: Map[Contaminant.Value, ContaminationLevel.Value],
+	/** Contaminants which must be cleaned from tips before entering this liquid */
+	val prohibitedTipContaminants: Seq[Contaminant.Value]
+)
+
 class Contamination(val bCells: Boolean, val bDna: Boolean, val bOtherContaminant: Boolean) {
 	def +(other: Contamination): Contamination = {
 		new Contamination(
