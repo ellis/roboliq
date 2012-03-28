37bd50fcd2f8661b021428ca1af66c6fc37d24d6
Ellis Whitehead <ellis@ellisw.net>
2012-03-28 22:17:21 +0200

    RoboliqYamlBean now handles plates, substances, and history

--- compiler/src/main/scala/temp/YamlTest3.scala
+++ compiler/src/main/scala/temp/YamlTest3.scala
@@ -14,7 +14,26 @@ import org.yaml.snakeyaml.introspector.Property
 import org.yaml.snakeyaml.nodes.NodeTuple
 import org.yaml.snakeyaml.TypeDescription
 
-class History {
+
+class RoboliqYamlBean {
+	@BeanProperty var plates: java.util.LinkedHashMap[String, PlateBean] = null
+	@BeanProperty var substances: java.util.LinkedHashMap[String, java.util.List[SubstanceItem]] = null
+	@BeanProperty var history: java.util.LinkedHashMap[String, java.util.List[HistoryItem]] = null
+}
+
+class PlateBean {
+	@BeanProperty var model: String = null
+	@BeanProperty var description: String = null
+	@BeanProperty var barcode: String = null
+}
+
+trait SubstanceItem
+
+class SubstanceItemDnaBean extends SubstanceItem {
+	@BeanProperty var sequence: String = null
+}
+
+class HistoryBean {
 	@BeanProperty var history: java.util.LinkedHashMap[String, java.util.List[HistoryItem]] = null
 }
 
@@ -41,7 +60,8 @@ object HistoryItemAddBean {
 }
 
 class MyRepresenter extends Representer {
-	addClassTag(classOf[History], new Tag("!history"));
+	//addClassTag(classOf[HistoryBean], new Tag("!history"));
+	addClassTag(classOf[SubstanceItemDnaBean], new Tag("!dna"));
 	addClassTag(classOf[HistoryItemAddBean], new Tag("!add"));
 
 	protected override def representJavaBeanProperty(
@@ -59,7 +79,8 @@ class MyRepresenter extends Representer {
 }
 
 class MyConstructor extends Constructor {
-	addTypeDescription(new TypeDescription(classOf[History], "!history"))
+	//addTypeDescription(new TypeDescription(classOf[HistoryBean], "!history"))
+	addTypeDescription(new TypeDescription(classOf[SubstanceItemDnaBean], "!dna"))
 	addTypeDescription(new TypeDescription(classOf[HistoryItemAddBean], "!add"))
 }
 
@@ -75,7 +96,7 @@ class YamlTest3 {
 	
 	val wellHistories = new java.util.LinkedHashMap[String, java.util.List[HistoryItem]]
 	wellHistories.put("E2215(A01)", l)
-	val history = new History
+	val history = new HistoryBean
 	history.history = wellHistories
 	
 	def run {
@@ -91,7 +112,8 @@ class YamlTest4 {
 
 	val s0 = scala.io.Source.fromFile("database3.yaml").mkString
 	//val o0 = yamlIn.loadAs(s0, classOf[java.util.LinkedHashMap[String, java.util.List[HistoryItem]]])
-	val o0 = yamlIn.loadAs(s0, classOf[History])
+	//val o0 = yamlIn.loadAs(s0, classOf[HistoryBean])
+	val o0 = yamlIn.loadAs(s0, classOf[RoboliqYamlBean])
 	
 	def run {
 		println(yamlOut.dump(o0))
