c8864b5acc63daab4024cda9789de7e7b1451380
Ellis Whitehead <ellis@ellisw.net>
2012-04-12 12:02:57 +0200

    working on protocol-007

--- bsse/src/main/scala/roboliq/labs/bsse/BsseMain.scala
+++ bsse/src/main/scala/roboliq/labs/bsse/BsseMain.scala
@@ -86,8 +86,8 @@ class YamlTest2(args: List[String]) {
 
 object BsseMain {
 	def main(args: Array[String]) {
-		new roboliq.labs.bsse.examples.PrimerTest1().run()
-		//new YamlTest2(args.toList).run
+		//new roboliq.labs.bsse.examples.PrimerTest1().run()
+		new YamlTest2(args.toList).run
 	}
 
 	/*
--- bsse/src/main/scala/roboliq/labs/bsse/examples/PrimerTest1.scala
+++ bsse/src/main/scala/roboliq/labs/bsse/examples/PrimerTest1.scala
@@ -11,11 +11,11 @@ class PrimerTest1 {
 	val idPlatePrimer = "E2215"
 	
 	println("B")
-	private def combine(sTemplateKey: String, s1a: Int, s1b: Int, s2a: Int, s2b: Int): List[PcrProductBean] = {
+	private def combine(sPart: String, template: String, s1a: Int, s1b: Int, s2a: Int, s2b: Int): List[PcrProductBean] = {
 		println("C")
 		val lProduct = (for (i1 <- s1a to s1b; i2 <- s2a to s2b) yield {
 			val product = new PcrProductBean
-			product.template = "FRP000"
+			product.template = template
 			product.forwardPrimer = "SEQUENCE_"+df.format(i1+1)
 			product.backwardPrimer = "SEQUENCE_"+df.format(i2+1)
 			product
@@ -25,13 +25,17 @@ class PrimerTest1 {
 	
 	def run() {
 		val lProduct: List[PcrProductBean] = 
-			combine("pAct1", 0, 6, 7, 16) ++
-			combine("lacI-NLS-STOP", 17, 28, 29, 36) ++
-			combine("cyc1Term", 37, 44, 45, 48)
+			combine("pAct1",         "FRP572", 0, 6, 7, 16) ++
+			combine("lacI-NLS-STOP", "FRP128", 17, 28, 29, 36) ++
+			combine("cyc1Term",      "FRP572", 37, 44, 45, 48)
 		lProduct.foreach(println)
 		
 		val lProduct96 = lProduct.take(96)
 		val lSrc1 = lProduct96.map(_.template)
+		val lSrc2 = lProduct96.map(_.forwardPrimer)
+		val lSrc3 = lProduct96.map(_.backwardPrimer)
 		println("  src: "+lSrc1.mkString(","))
+		println("  src: "+lSrc2.mkString(","))
+		println("  src: "+lSrc3.mkString(","))
 	}
 }
