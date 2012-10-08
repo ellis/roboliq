9b6f7707ef9c8ecd357d4c58a9c98ac24a16e0ac
Ellis Whitehead <ellis@ellisw.net>
2012-10-08 20:23:24 +0200

    .

--- testdata/dyes-201210/dyes.r
+++ testdata/dyes-201210/dyes.r
@@ -3,6 +3,35 @@
 #json <- fromJSON(paste(readLines('read10times1a.json'), collapse=""))
 #df <- as.data.frame(t(sapply(json, unlist)))
 
+library('psych')
+library('scatterplot3d')
+
 setwd("~/src/roboliq/testdata/dyes-201210")
-df1a <- read.delim('read10times1a.tab', header=1)
-df1b <- read.delim('read10times1b.tab', header=1)
+
+relValue = function(values) {
+  m = median(values)
+  (values - m) / m
+}
+
+df <- read.delim('all.tab', header=1)
+dfwell <- (df$row - 1) * 12 + df$col
+
+# Compare dye13 and dye14
+par(mfrow=c(2,2))
+plot(df$readout[df$id == 'dye13Aa'], main="10ul multi/air", ylab='readout')
+plot(df$readout[df$id == 'dye13Ab'], main="10ul single/air", ylab='readout')
+plot(df$readout[df$id == 'dye14Aa'], main="10ul multi/wet", ylab='readout')
+plot(df$readout[df$id == 'dye14Ab'], main="10ul single/wet", ylab='readout')
+par(mfrow=c(2,2))
+plot(dfwell[df$id == 'dye13Aa'], relValue(df$readout[df$id == 'dye13Aa']), main="10ul multi/air", ylab='rel. readout')
+plot(dfwell[df$id == 'dye13Ab'], relValue(df$readout[df$id == 'dye13Ab']), main="10ul single/air", ylab='rel. readout')
+plot(dfwell[df$id == 'dye14Aa'], relValue(df$readout[df$id == 'dye14Aa']), main="10ul multi/wet", ylab='rel. readout')
+plot(dfwell[df$id == 'dye14Ab'], relValue(df$readout[df$id == 'dye14Ab']), main="10ul single/wet", ylab='rel. readout')
+
+# Test reader reliability (it appears to be very good)
+reader <- read.delim('reader.tab', header=1)
+well <- (reader$row - 1) * 12 + reader$col
+describe.by(reader$readout, well)
+boxplot(reader$readout[reader$vol == 10] ~ well[reader$vol == 10])
+boxplot(reader$readout[reader$vol == 20] ~ well[reader$vol == 20])
+boxplot(reader$readout[reader$vol == 10] ~ well[reader$vol == 10], ylim=c(0, max(reader$readout[reader$vol == 10])))
