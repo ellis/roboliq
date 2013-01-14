be2a322e718e85b3a94fab70bf1b6b910bf07fa1
Ellis Whitehead <ellis@ellisw>
2013-01-14 21:40:29 +0100

    .

--- utils0/src/main/scala/roboliq/utils0/applicative/ApplicativeMain.scala
+++ utils0/src/main/scala/roboliq/utils0/applicative/ApplicativeMain.scala
@@ -3,17 +3,33 @@ package roboliq.utils0.applicative
 import scalaz._
 import Scalaz._
 
+class Environment(
+	val obj_m: Map[String, String],
+	val cmd_m: Map[String, String]
+)
+
 object ApplicativeMain extends App {
-	val obj_m = Map("P1" -> "Plate1", "water" -> "Water")
-	def cmd_m = Map("plate" -> "P1", "liquid" -> "water")
 	
-	def getParam(id: String): Option[String] = cmd_m.get(id)
-	def getPlate(id: String): Option[String] = obj_m.get(id)
-	def getLiquid(id: String): Option[String] = obj_m.get(id)
+	def getParam(id: String)(implicit env: Environment): Option[String] = env.cmd_m.get(id)
+	def getPlate(id: String)(implicit env: Environment): Option[String] = env.obj_m.get(id)
+	def getLiquid(id: String)(implicit env: Environment): Option[String] = env.obj_m.get(id)
 	
-	(getParam("plate") |@| getParam("liquid")) { (plateId, liquidId) =>
-		(getPlate(plateId) |@| getLiquid(liquidId)) { (plate, liquid) =>
-			println(plate, liquid)
+	val env1 = new Environment(
+		Map("P1" -> "Plate1", "water" -> "Water"),
+		Map("plate" -> "P1", "liquid" -> "water")
+	)
+	
+	def makeit(): (Environment => Unit) = {
+		def x(env0: Environment): Unit = {
+			implicit val env = env0
+			(getParam("plate") |@| getParam("liquid")) { (plateId, liquidId) =>
+				(getPlate(plateId) |@| getLiquid(liquidId)) { (plate, liquid) =>
+					println(plate, liquid)
+				}
+			}
 		}
+		x
 	}
+	
+	makeit()(env1)
 }
