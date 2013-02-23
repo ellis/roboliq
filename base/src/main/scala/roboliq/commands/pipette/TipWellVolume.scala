56a07a9d25f720a8b41ab5652ceb72a2fa1318d3
Ellis Whitehead <ellis@ellisw>
2013-02-23 22:43:39 +0100

    Turn TipWellVolumePolicy family of classes into case classes

--- base/src/main/scala/roboliq/commands/pipette/TipWellVolume.scala
+++ base/src/main/scala/roboliq/commands/pipette/TipWellVolume.scala
@@ -9,40 +9,51 @@ trait HasVolume { val volume: LiquidVolume }
 trait HasPolicy { val policy: PipettePolicy }
 trait HasMixSpec { val mixSpec: MixSpec }
 
+trait HasTipWell extends HasTip with HasWell
+trait HasTipWellVolume extends HasTipWell with HasVolume
+trait HasTipWellPolicy extends HasTipWell with HasPolicy
+trait HasTipWellVolumePolicy extends HasTipWellVolume with HasPolicy
+
 //class TW(val tip: Tip, val well: Well) extends HasTip with HasWell
 //class TWV(val tip: Tip, val well: Well, val volume: LiquidVolume) extends HasTip with HasWell with HasVolume
 //class TWVP(val tip: Tip, val well: Well, val volume: LiquidVolume, val policy: String) extends HasTip with HasWell with HasVolume with HasPolicy
 
-//trait HasTipWellPolicy extends HasTip with HasWell with HasPolicy
-//trait HasTipWell extends HasTip with HasWell
-//trait HasTipWellVolume extends HasTipWell with HasVolume
-//trait HasTipWellVolumePolicy extends HasTipWellVolume with HasPolicy
-
-sealed class TipWell(val tip: Tip, val well: Well) extends HasTip with HasWell {
-	override def toString = "TipWell("+(tip.index+1)+","+well+")" 
+case class TipWell(
+	tip: Tip,
+	well: Well
+) extends HasTipWell {
+	override def toString = s"TipWell(${tip.id},${well.id})"
 }
 
-sealed class TipWellVolume(
-		tip: Tip, well: Well,
-		val volume: LiquidVolume
-	) extends TipWell(tip, well) with HasVolume {
-	override def toString = "TipWellVolume("+tip.index+","+well.id+","+volume+")"
+case class TipWellVolume(
+	tip: Tip,
+	well: Well,
+	volume: LiquidVolume
+) extends HasTipWellVolume {
+	override def toString = s"TipWellVolume(${tip.id},${well.id},$volume)"
 }
 
-sealed class TipWellPolicy(tip: Tip, well: Well,
-		val policy: PipettePolicy
-	) extends TipWell(tip, well) with HasPolicy {
-	override def toString = "TipWellPolicy("+tip.index+","+well.id+","+policy+")"
+case class TipWellPolicy(
+	tip: Tip,
+	well: Well,
+	policy: PipettePolicy
+) extends HasTipWellPolicy {
+	override def toString = s"TipWellPolicy(${tip.id},${well.id},$policy)"
 }
 
-sealed class TipWellVolumePolicy(tip: Tip, well: Well, volume: LiquidVolume,
-		val policy: PipettePolicy
-	) extends TipWellVolume(tip, well, volume) with HasPolicy {
-	override def toString = "TipWellVolumePolicy("+tip.index+","+well.id+","+volume+","+policy+")"
+case class TipWellVolumePolicy(
+	tip: Tip,
+	well: Well,
+	volume: LiquidVolume,
+	policy: PipettePolicy
+) extends HasTipWellVolumePolicy {
+	override def toString = s"TipWellVolumePolicy(${tip.id},${well.id},$volume,$policy)"
 }
 
-sealed class TipWellMix(tip: Tip, well: Well,
-		val mixSpec: MixSpec
-	) extends TipWell(tip, well) with HasMixSpec {
-	override def toString = "TipWellMix("+tip.index+","+well.id+","+mixSpec+")"
+case class TipWellMix(
+	tip: Tip,
+	well: Well,
+	mixSpec: MixSpec
+) extends HasTipWell with HasMixSpec {
+	override def toString = "TipWellMix("+tip.id+","+well.id+","+mixSpec+")"
 }
