159df2f76ecf70effca2e88004897f820d0beb74
Ellis Whitehead <ellis@server.fake>
2013-06-20 13:29:57 +0200

    pc: pipette then thermocycle

--- tasks/5/pc.lisp
+++ tasks/5/pc.lisp
@@ -1,4 +1,4 @@
-; Perform pipette2 task
+; Perform pipette2 task then thermocycle-plate
 (defproblem pc domain
  ; initial conditions
  (
@@ -14,11 +14,12 @@
   (is-site s1)
   (is-site s2)
   (is-site sealerSite)
+  (is-site thermocyclerSite)
   (is-plate p1)
   (is-plate p2)
   (is-plateModel m1)
   (is-plateModel m2)
-  (is-thermocycleSpec thermocycleSpec1)
+  (is-thermocyclerSpec thermocyclerSpec1)
 
   ; user
   (agent-is-active user)
@@ -34,10 +35,12 @@
   (agent-has-arm r1 r1arm)
   (agent-has-pipetter r1 pipetter)
   (agent-has-sealer r1 sealer)
+  (agent-has-thermocycler r1 thermocycler)
   ; robot arm
   (arm-can-plateModel r1arm m1)
   (arm-can-site r1arm s1)
   (arm-can-site r1arm sealerSite)
+  (arm-can-site r1arm thermocyclerSite)
   ; pipetter
   (pipetter-can-plateModel pipetter m1)
   (pipetter-can-plateModel pipetter m2)
@@ -46,12 +49,15 @@
   ; sealer
   (sealer-site sealer sealerSite)
   (sealer-can-plateModel sealer m1)
+  ; thermocycler
+  (thermocycler-site thermocycler thermocyclerSite)
   ; sites
   (site-can-plateModel offsite m1)
   (site-can-plateModel offsite m2)
   (site-can-plateModel s1 m1)
   (site-can-plateModel s2 m2)
   (site-can-plateModel sealerSite m1)
+  (site-can-plateModel thermocyclerSite m1)
 
   ; Initial state of labware
   (plate-model p1 m1)
@@ -62,7 +68,14 @@
  ; tasks
  (
   (pipette2 p1 p2)
-  (seal-plate p1)
-  ;(thermocycle-plate )
+  (thermocycle-plate thermocycler thermocyclerSpec1 p1 s1)
+   ;(seal-plate p1)
+   ;(thermocycler-open thermocycler)
+   ;(set-plate-site p1 m1 thermocyclerSite)
+   ;(thermocycler-close thermocycler)
+   ;(!thermocycler-run r1 thermocycler thermocyclerSpec1)
+   ;(thermocycler-open thermocycler)
+   ;(set-plate-site p1 m1 s1)
+   ;(thermocycler-close thermocycler)
  )
 )
