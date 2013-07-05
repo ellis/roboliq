47544b5b4c1cca306b3e343fb9fb82752d2c1977
ellis <ellis@ellisw.net>
2013-07-05 12:49:13 +0200

    use two robots

--- tasks/6/pa.lisp
+++ tasks/6/pa.lisp
@@ -1,70 +1,73 @@
-; Perform pipette2 task
-(defproblem pb domain
+; Perform pipette1 task
+(defproblem pa domain
  ; initial conditions
  (
   ; types
   (is-agent user)
   (is-arm userArm)
   (is-agent r1)
+  (is-agent r2)
   (is-arm r1arm)
+  (is-arm r2arm)
+  (is-arm transportArm)
   (is-pipetter pipetter)
   (is-sealer sealer)
   (is-shaker shaker)
   (is-site offsite)
   (is-site s1)
-  (is-site s2)
   (is-site sealerSite)
   (is-plate p1)
-  (is-plate p2)
   (is-plateModel m1)
-  (is-plateModel m2)
+  (is-shakerSpec shakerSpec1)
 
   ; user
   (agent-is-active user)
   (agent-has-arm user userArm)
   ; user arm
   (arm-can-plateModel userArm m1)
-  (arm-can-plateModel userArm m2)
   (arm-can-site userArm offsite)
   (arm-can-site userArm s1)
-  (arm-can-site userArm s2)
   ; robot r1
   (agent-is-active r1)
   (agent-has-arm r1 r1arm)
+  (agent-has-arm r1 transportArm)
   (agent-has-pipetter r1 pipetter)
-  (agent-has-sealer r1 sealer)
-  ; robot arm
+  ; robot r2
+  (agent-is-active r2)
+  (agent-has-arm r2 r2arm)
+  (agent-has-sealer r2 sealer)
+  ; robot 1 arm
   (arm-can-plateModel r1arm m1)
   (arm-can-site r1arm s1)
+  ; robot 2 arm
+  (arm-can-plateModel r2arm m1)
   (arm-can-site r1arm sealerSite)
+  ; transport arm
+  (arm-can-plateModel transportArm m1)
+  (arm-can-site transportArm s1)
+  (arm-can-site transportArm sealerSite)
   ; pipetter
   (pipetter-can-plateModel pipetter m1)
-  (pipetter-can-plateModel pipetter m2)
   (pipetter-can-site pipetter s1)
-  (pipetter-can-site pipetter s2)
   ; sealer
   (sealer-site sealer sealerSite)
   (sealer-can-plateModel sealer m1)
   ; sites
   (site-can-plateModel offsite m1)
-  (site-can-plateModel offsite m2)
   (site-can-plateModel s1 m1)
-  (site-can-plateModel s2 m2)
   (site-can-plateModel sealerSite m1)
 
   ; Initial state of labware
   (plate-model p1 m1)
   (plate-site p1 offsite)
-  (plate-model p2 m2)
-  (plate-site p2 offsite)
  )
  ; tasks
  (
-  ;(set-plate-site p1 m1 s1) ; works
-  ;(set-plate-site p2 m2 s2) ; works
-  ;(!pipette2 r1 pipetter p1 m1 s1 p2 m2 s2)
-  ;(pipette2-adpms r1 pipetter p1 m1 s1 p2 m2 s2)
+  ;(set-plate-site p1 m1 s1)
+  ;(!pipette1 r1 pipetter p1 m1 s1)
+  ;(pipette1-adpms r1 pipetter p1 m1 s1) ; works
   ;(pipette1-pm p1 m1) ; works
-  (pipette2 p1 p2)
+  (pipette1 p1)
+  (seal-plate p1)
  )
 )
---
+++ tasks/readme.md
@@ -0,0 +1,8 @@
+## 5
+
+Goal is to pipette, seal, thermocycle
+
+## 6
+
+Goal is to use two robots.  Pipette on robot 1, then seal on robot 2.
+
