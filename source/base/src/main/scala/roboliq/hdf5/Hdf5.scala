8585c529ad2416ada4bc8cf30a837326dfab0de5
Ellis Whitehead <ellis@server.fake>
2014-07-22 15:27:25 +0200

    Now writing out instructions in a format that R can read

--- source/base/src/main/scala/roboliq/hdf5/Hdf5.scala
+++ source/base/src/main/scala/roboliq/hdf5/Hdf5.scala
@@ -10,15 +10,46 @@ import roboliq.entities.SubstanceUnits
 import java.io.File
 import ch.systemsx.cisd.hdf5.HDF5GenericStorageFeatures
 import java.io.ByteArrayOutputStream
+import java.nio.ByteBuffer
+import java.nio.ByteBufferAsIntBufferL
+import java.io.DataOutputStream
+
+class Hdf5Stream(os: ByteArrayOutputStream) {
+	def putAscii(s: String, length: Int) {
+		putBytesPlusNull(s.getBytes("ASCII"), length)
+	}
+
+	def putUtf8(s: String, length: Int) {
+		// FIXME: this could lead to an error in which a multi-byte UTF-8 code gets truncated
+		putBytesPlusNull(s.getBytes("UTF-8"), length)
+	}
+	
+	private def putBytesPlusNull(l: Array[Byte], length: Int) {
+		putBytes(l, length - 1)
+		os.write(0)
+	}
+	
+	def putBytes(l: Array[Byte], length: Int) {
+		val n = math.min(length, l.length)
+		os.write(l, 0, n)
+		for (i <- n until length)
+			os.write(0)
+	}
+	
+	def putInt32(x: Int) {
+		val buf = ByteBuffer.allocate(4)
+		buf.order(java.nio.ByteOrder.LITTLE_ENDIAN)
+		buf.putInt(x)
+		putBytes(buf.array(), 4)
+	}
+}
 
 case class InstructionEntry(
 	var scriptId: String,
 	var instructionIndex: Int,
 	var agent: String,
 	var description: String
-) {
-	def this() = this("", 0, "", "")
-}
+)
 
 object InstructionEntry {
 	def getHDF5Type(reader: IHDF5CompoundWriter): HDF5CompoundType[InstructionEntry] = {
@@ -30,6 +61,59 @@ object InstructionEntry {
 			mapping("description").length(256)
 		)
 	}
+	
+	def createHdf5Type(): (Int, List[Int]) = {
+		import ncsa.hdf.hdf5lib.H5
+		import ncsa.hdf.hdf5lib.HDF5Constants
+		
+		val typeCompound = H5.H5Tcreate(HDF5Constants.H5T_COMPOUND, 34+4+26+256);
+		val typeString1 = H5.H5Tcreate(HDF5Constants.H5T_STRING, 34);
+		val typeString2 = H5.H5Tcreate(HDF5Constants.H5T_STRING, 26);
+		val typeString3 = H5.H5Tcreate(HDF5Constants.H5T_STRING, 256);
+		
+		H5.H5Tset_strpad(typeString1, HDF5Constants.H5T_STR_NULLTERM)
+		H5.H5Tset_cset(typeString1, HDF5Constants.H5T_CSET_ASCII)
+
+		H5.H5Tset_strpad(typeString2, HDF5Constants.H5T_STR_NULLTERM)
+		H5.H5Tset_cset(typeString2, HDF5Constants.H5T_CSET_UTF8)
+
+		H5.H5Tset_strpad(typeString3, HDF5Constants.H5T_STR_NULLTERM)
+		H5.H5Tset_cset(typeString3, HDF5Constants.H5T_CSET_UTF8)
+
+		H5.H5Tinsert(typeCompound, "scriptId", 0, typeString1)
+		H5.H5Tinsert(typeCompound, "instructionIndex", 0+34, HDF5Constants.H5T_STD_I32LE)
+		H5.H5Tinsert(typeCompound, "agent", 0+34+4, typeString2)
+		H5.H5Tinsert(typeCompound, "description", 0+34+4+26, typeString3)
+		
+		(typeCompound, List(typeString1, typeString2, typeString3))
+	}
+
+	def createByteArray(entry_l: Iterable[InstructionEntry]): Array[Byte] = {
+		val bs = new ByteArrayOutputStream((34+4+26+256) * entry_l.size)
+		val hs = new Hdf5Stream(bs)
+		for (entry <- entry_l) {
+			hs.putAscii(entry.scriptId, 34)
+			hs.putInt32(entry.instructionIndex)
+			hs.putUtf8(entry.agent, 26)
+			hs.putUtf8(entry.description, 256)
+			/*hs.putAscii("123456789012345678901234567890123", 34)
+			hs.putInt32(0x01020304)
+			hs.putUtf8("abcdefghijklmnopqrstuvwxyz", 26)
+			hs.putUtf8("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 256)*/
+		}
+		val x = bs.toByteArray()
+		//x.toList.take(34+4+26+256).zipWithIndex.foreach(println)
+		x
+	}
+	
+	/*
+	def saveToStream(entry: InstructionEntry, os: ByteArrayOutputStream) {
+		val os2 = new Hdf5Stream(os)
+		os2.putAscii(entry.scriptId, 34)
+		os2.putInt32(entry.instructionIndex)
+		os2.putUtf8(entry.agent, 26)
+		os2.putUtf8(entry.description, 256)
+	}*/
 }
 
 case class WellDispenseEntry(
@@ -42,9 +126,6 @@ case class WellDispenseEntry(
 	var agent: String,
 	var tip: Int
 ) {
-	def streamToBytes(os: ByteArrayOutputStream) {
-		os.write(x$1)
-	}
 }
 
 object WellDispenseEntry {
@@ -142,19 +223,31 @@ class Hdf5(
 		val space3 = H5.H5Screate_simple(1, Array(2L), Array(2L))
 		val data3 = H5.H5Dcreate(file, "data3", type3, space3, linkCreationPropertyList, dataSetCreationPropertyListId, HDF5Constants.H5P_DEFAULT)
 		H5.H5Dwrite(data3, type3, HDF5Constants.H5S_ALL, HDF5Constants.H5S_ALL, HDF5Constants.H5P_DEFAULT, Array[Byte](1, 0, 0, 0,  'H', 'i', 0, 0,  2, 0, 0, 0,  'B', 'a', 'b', 0))
+		
+		val (type4, more4) = InstructionEntry.createHdf5Type()
+		val space4 = H5.H5Screate_simple(1, Array[Long](entry_l.length), Array[Long](entry_l.length))
+		val data4 = H5.H5Dcreate(file, "instructions", type4, space4, linkCreationPropertyList, dataSetCreationPropertyListId, HDF5Constants.H5P_DEFAULT)
+		val byte4_l = InstructionEntry.createByteArray(entry_l)
+		H5.H5Dwrite(data4, type4, HDF5Constants.H5S_ALL, HDF5Constants.H5S_ALL, HDF5Constants.H5P_DEFAULT, byte4_l)
+		
 
 		H5.H5Dclose(data1)
 		H5.H5Dclose(data2)
 		H5.H5Dclose(data3)
+		H5.H5Dclose(data4)
 		H5.H5Pclose(dataSetCreationPropertyListId)
 		H5.H5Pclose(linkCreationPropertyList)
 		H5.H5Tclose(type2)
 		H5.H5Tclose(type3)
 		H5.H5Tclose(type3b)
+		H5.H5Tclose(type4)
+		more4.foreach(H5.H5Tclose)
 		H5.H5Sclose(space1)
 		H5.H5Sclose(space2)
 		H5.H5Sclose(space3)
+		H5.H5Sclose(space4)
 		H5.H5Fclose(file)
+		// TODO: Also need to close the string types
 	}
 	
 	/*def addWellMixtures(scriptId: String, instruction_l: List[AgentInstruction]) {
