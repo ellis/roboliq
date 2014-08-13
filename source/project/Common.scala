7cca43dc461f9b8b06b92ead37cb0b985e991685
Ellis Whitehead <ellis.whitehead@bsse.ethz.ch>
2014-08-13 13:33:46 +0200

    Update to scala 2.11.2, add sbt.bat for windows, change paths in roboliq.yaml for windows

--- source/project/Common.scala
+++ source/project/Common.scala
@@ -4,7 +4,7 @@ import sbt.Keys._
 object RoboliqSettings {
 	val buildOrganization = "bsse.ethz.ch"
 	val buildVersion      = "0.1"
-	val buildScalaVersion = "2.10.3"
+	val buildScalaVersion = "2.11.2"
 
 	import RoboliqDeps._
 
@@ -24,20 +24,20 @@ object RoboliqSettings {
 
 	// Dependencies
 	object RoboliqDeps {
-		val compiler = "org.scala-lang" % "scala-compiler" % "2.10.3"
-		val reflect = "org.scala-lang" % "scala-reflect" % "2.10.3"
+		val compiler = "org.scala-lang" % "scala-compiler" % "2.11.2"
+		val reflect = "org.scala-lang" % "scala-reflect" % "2.11.2"
 		val commons_io = "commons-io" % "commons-io" % "2.2"
-		val scalaz = "org.scalaz" % "scalaz-core_2.10" % "7.0.0-M7"
-		val grizzled = "org.clapper" % "grizzled-slf4j_2.10" % "1.0.1"
+		val scalaz = "org.scalaz" %% "scalaz-core" % "7.1.0"
+		val grizzled = "org.clapper" %% "grizzled-slf4j" % "1.0.2"
 		val logback = "ch.qos.logback" % "logback-classic" % "1.0.7"
-		val scalatest = "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
-		val akka = "com.typesafe.akka" %% "akka-actor" % "2.1.0"
+		val scalatest = "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
+		//val akka = "com.typesafe.akka" %% "akka-actor" % "2.1.0"
 		val scopt = "com.github.scopt" %% "scopt" % "3.2.0"
 		val yaml = "org.yaml" % "snakeyaml" % "1.10"
 		val ejml = "com.googlecode.efficient-java-matrix-library" % "ejml" % "0.16"
 		val json_gson = "com.google.code.gson" % "gson" % "2.2.1"
-		val json_spray = "io.spray" % "spray-json_2.10" % "1.2.3"
-		val scala_graph_core = "com.assembla.scala-incubator" % "graph-core_2.10" % "1.8.0"
+		val json_spray = "io.spray" %% "spray-json" % "1.2.6"
+		val scala_graph_core = "com.assembla.scala-incubator" %% "graph-core" % "1.9.0"
 		val saddle = "org.scala-saddle" %% "saddle-core" % "1.3.+"
 		val saddle_hdf5 = "org.scala-saddle" %% "saddle-hdf5" % "1.3.+"
 	}
--- source/project/plugins.sbt
+++ source/project/plugins.sbt
@@ -1,4 +1,4 @@
-//addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.0.0")
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.5.0")
 
 //resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
 
