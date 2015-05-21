3f2c750a1926433fdd457248bd95760d113bcae3
Ellis Whitehead <ellis@ellisw.net>
2015-05-21 14:06:24 +0200

    LabObjects thoughts

--- source/base2/src/main/scala/roboliq/input/LabObjects.scala
+++ source/base2/src/main/scala/roboliq/input/LabObjects.scala
@@ -1,5 +1,8 @@
 package roboliq.input
 
+import scala.language.dynamics
+import scala.collection.mutable.HashMap
+
 trait LabObject {
 	val `type`: String
 }
@@ -18,4 +21,41 @@ class SiteObject extends LabObject {
 
 class SiteModelObject extends LabObject {
 	val `type` = "SiteModel"
+}
+
+class LabObjects extends Dynamic {
+	var nameToValue_l = Vector[(String, RjsBasicValue)]()
+	
+	def set(name: String, value: RjsBasicValue) {
+		// Remove old values
+		val nameWithPeriod = name + "."
+		nameToValue_l = nameToValue_l.filter(pair =>
+			if (name == pair._1) false
+			else if (pair._1.startsWith(nameWithPeriod)) false
+			else true
+		)
+		// Add new value
+		add(name, value)
+	}
+	
+	private def add(prefix: String, value: RjsBasicValue) {
+		value match {
+			case m: RjsBasicMap =>
+				val prefix1 = prefix + "."
+				for ((name, value) <- m.map) {
+					add(prefix1+name, value)
+				}
+			case _ => nameToValue_l = nameToValue_l :+ (prefix, value)
+		}
+	}
+	
+	def get(name: String): Option[RjsBasicValue] = {
+		val nameWithPeriod = name + "."
+		val l = nameToValue_l.filter(pair =>
+			if (name == pair._1) true
+			else if (pair._1.startsWith(nameWithPeriod)) true
+			else false
+		)
+		???
+	}
 }
