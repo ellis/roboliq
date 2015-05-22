f6fcc41176caacc1e56f94a3a9cf7fe53647caf9
Ellis Whitehead <ellis@ellisw.net>
2015-05-22 12:13:13 +0200

    minor

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
 yaml = require('js-yaml');
-fs   = require('fs');
+fs = require('fs');
+_ = require('underscore');
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function handleStep(step) {
+	if (step.hasOwnProperty('command')) {
+		handleCommand(step);
+	_.each(step, function(value, key) {
+		if (isNumeric(key)) {
+		}
+	});
+}
+
+function handleCommand(cmd) {
 
 // Get document, or throw exception on error
 try {
-  var doc = yaml.safeLoad(fs.readFileSync('protocol1.yaml', 'utf8'));
-  console.log(JSON.stringify(doc));
+	var protocol = yaml.safeLoad(fs.readFileSync('protocol1.yaml', 'utf8'));
+	console.log(protocol);
+	
+	
 } catch (e) {
   console.log(e);
 }
