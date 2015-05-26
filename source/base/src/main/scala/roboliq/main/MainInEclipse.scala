fc86f12f4e1083eb4ea57ad76a625cd9822db269
Ellis Whitehead <ellis.whitehead@bsse.ethz.ch>
2015-05-26 10:11:44 +0200

    tania16

--- source/base/src/main/scala/roboliq/main/MainInEclipse.scala
+++ source/base/src/main/scala/roboliq/main/MainInEclipse.scala
@@ -152,5 +152,6 @@ object MainInEclipse extends App {
 	//runTemp("qa03_weight_3")
 	//runTemp("qa03_weight_4")
 	//runTemp("qa05_dye_vol_range")
-	runTemp("tania16_ph_2_pipette")
+	//runTemp("tania16_ph_2_pipette")
+	runTemp("tania16_ph_3_pipette2")
 }
