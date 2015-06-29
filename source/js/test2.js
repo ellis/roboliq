0bf4ebd602faba1382f064b2a0843763528e8f97
Ellis Whitehead <ellis@ellisw.net>
2015-06-29 11:31:44 +0200

    sealerExample (runs)

--- source/js/test2.js
+++ source/js/test2.js
@@ -22,73 +22,20 @@ console.log(x);
 console.log();
 */
 
-var sealerExample0 = [
-	// State
-	{
-		"isAgent": {"agent": "ourlab.mario.evoware"},
-		"isSealer": {"sealer": "sealer1"},
-		"isLabware": {"labware": "plate1"},
-		"isModel": {"model": "model1"},
-		"isModel": {"model": "siteModel1"},
-		"isPlate": {"labware": "plate1"},
-		"isSite": {"site": "site1"},
-		"isSiteModel": {"model": "siteModel1"},
-		"model": {"labware": "plate1", "model": "model1"},
-		"siteModel": {"site": "site1", "siteModel": "siteModel1"},
-		"stackable": {"below": "siteModel1", "above": "model1"},
-		"location": {"labware": "plate1", "site": "site1"}
-	},
-
-	// Tasks
-	{"tasks": {"ordered": [
-		//{"sealPlate": {"labware": "plate1"}}
-		{"sealAction": {"agent": "ourlab.mario.evoware", "equipment": "sealer1", "program": "someprogram", "labware": "plate1", "model": "model1", "site": "site1", "siteModel": "siteModel1"}}
-	]}},
-
-	// Actions
-	{"action": {"description": "fully specified seal command",
-		"task": {"sealAction": {"agent": "?agent", "equipment": "?equipment", "program": "?program", "labware": "?labware", "model": "?model", "site": "?site", "siteModel": "?siteModel"}},
-		"preconditions": [
-			{"isAgent": {"agent": "?agent"}},
-			{"isSealer": {"equipment": "?equipment"}},
-			{"isPlate": {"labware": "?labware"}},
-			{"isModel": {"model": "?model"}},
-			{"isSiteModel": {"model": "?siteModel"}},
-			{"location": {"labware": "?labware", "site": "?site"}}
-		],
-		"deletions": [],
-		"additions": []
-	}}/*,
-
-   // Methods
-   {"method": {"description": "drop thing1 and pick up thing2",
-	       "task": {"sealPlate": {"labware": "?labware"}},
-	       "preconditions": [{"have": {"agent": "?agent", "thing": "?thing1"}}],
-	       "subtasks": {"ordered": [{"drop": {"agent": "?agent", "thing": "?thing1"}},
-                                        {"pickup": {"agent": "?agent", "thing": "?thing2"}}]}}},
-   {"method": {"description": "drop thing2 and pick up thing1",
-	       "task": {"swap": {"agent": "?agent",
-				 "thing1": "?thing1",
-				 "thing2": "?thing2"}},
-	       "preconditions": [{"have": {"agent": "?agent", "thing": "?thing2"}}],
-	       "subtasks": {"ordered": [{"drop": {"agent": "?agent", "thing": "?thing2"}},
-                                        {"pickup": {"agent": "?agent", "thing": "?thing1"}}]}}},*/
-   // Rules: no rules in this example
-];
-
 var sealerExample = [
 	// State
 	{"isAgent": {"agent": "ourlab.mario.evoware"}},
-	{"isSealer": {"equipment": "sealer1"}},
+	{"isSealer": {"equipment": "ourlab.mario.sealer"}},
 	{"isLabware": {"labware": "plate1"}},
 	{"isModel": {"model": "model1"}},
 	{"isModel": {"model": "siteModel1"}},
 	{"isPlate": {"labware": "plate1"}},
 	{"isSite": {"site": "site1"}},
 	{"isSiteModel": {"model": "siteModel1"}},
-	{"model": {"labware": "plate1", "model": "model1"}},
 	{"siteModel": {"site": "site1", "siteModel": "siteModel1"}},
 	{"stackable": {"below": "siteModel1", "above": "model1"}},
+	{"canAgentEquipmentProgramModelSite": {"agent": "ourlab.mario.evoware", "equipment": "ourlab.mario.sealer", "program": "sealerProgram1", "model": "model1", "site": "site1"}},
+	{"model": {"labware": "plate1", "model": "model1"}},
 	{"location": {"labware": "plate1", "site": "site1"}},
 
 	// Tasks
@@ -98,32 +45,25 @@ var sealerExample = [
 
 	// Actions
 	{"action": {"description": "fully specified seal command",
-		"task": {"sealAction": {"agent": "?agent", "equipment": "?equipment", "program": "?program", "labware": "?labware", "model": "?model", "site": "?site", "siteModel": "?siteModel"}},
+		"task": {"sealAction": {"agent": "?agent", "equipment": "?equipment", "program": "?program", "labware": "?labware", "model": "?model", "site": "?site"}},
 		"preconditions": [
-			{"isAgent": {"agent": "?agent"}},
-			{"isSealer": {"equipment": "?equipment"}},
-			{"isPlate": {"labware": "?labware"}},
-			{"isModel": {"model": "?model"}},
-			{"isSiteModel": {"model": "?siteModel"}},
+			{"canAgentEquipmentProgramModelSite": {"agent": "?agent", "equipment": "?equipment", "program": "?program", "model": "?model", "site": "?site"}},
+			{"model": {"labware": "?labware", "model": "?model"}},
 			{"location": {"labware": "?labware", "site": "?site"}}
 		],
 		"deletions": [],
-		"additions": []
+		"additions": [{"plateHasSeal": {"labware": "?labware"}}]
 	}},
 
 	// Methods
 	{"method": {"description": "method for sealing",
 		"task": {"sealPlate": {"labware": "?labware"}},
 		"preconditions": [
-			{"isAgent": {"agent": "?agent"}},
-			{"isSealer": {"equipment": "?equipment"}},
-			{"isPlate": {"labware": "?labware"}},
-			{"isModel": {"model": "?model"}},
-			{"isSiteModel": {"model": "?siteModel"}},
-			{"location": {"labware": "?labware", "site": "?site"}}
+			{"canAgentEquipmentProgramModelSite": {"agent": "?agent", "equipment": "?equipment", "program": "?program", "model": "?model", "site": "?site"}},
+			{"model": {"labware": "?labware", "model": "?model"}}
 		],
 		"subtasks": {"ordered": [
-			{"sealAction": {"agent": "?agent", "equipment": "?equipment", "program": "program1", "labware": "?labware", "model": "?model", "site": "?site", "siteModel": "?siteModel"}}
+			{"sealAction": {"agent": "?agent", "equipment": "?equipment", "program": "?program", "labware": "?labware", "model": "?model", "site": "?site"}}
 		]}
 	}}
 	/*,
