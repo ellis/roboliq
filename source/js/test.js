d31e190e20817d40f1fbf7a895fab1ea053d5998
Ellis Whitehead <ellis@ellisw.net>
2015-07-17 14:25:59 +0200

    more work on combinatorial stuff

--- source/js/test.js
+++ source/js/test.js
@@ -56,40 +56,43 @@ var spec5 = {
 };
 
 var spec6 = {
-	list: [
-		{objects: [{source: "saltwater", volumes: "40ul"}]},
-		{merge: [
-			{objects: [{source: "sfGFP"}, {source: "tdGFP"}]},
-			{objects: [{volumes: "5ul"}]}
+	COMBINATORIAL_ARRAY: [
+		{source: "saltwater", volumes: "40ul"},
+		{COMBINATORIAL_MERGE: [
+			[{source: "sfGFP"}, {source: "tdGFP"}],
+			{volumes: "5ul"}
 		]}
 	]
 };
 
 function gen2(spec) {
 	console.log("gen2", spec);
-	if (spec.merge) {
-		return genMerge(spec.merge);
+	if (spec.COMBINATORIAL_MERGE) {
+		return genMerge(spec.COMBINATORIAL_MERGE);
 	}
-	else if (spec.list) {
-		return genList(spec.list);
+	else if (spec.COMBINATORIAL_ARRAY) {
+		return genList(spec.COMBINATORIAL_ARRAY);
 	}
-	else if (spec.objects) {
-		assert(_.isArray(spec.objects));
-		return spec.objects;
-		/*_.forEach(spec.objects, function(obj1) {
-			var obj2 = _.merge({}, obj0, obj1);
-			gen1(spec, obj1, index + 1, acc);
-		}*/
+	else {
+		return spec;
 	}
 }
 
 function genMerge(merges) {
 	assert(_.isArray(merges));
-	var lists = _.map(merges, function(elem) { return gen2(elem); });
-	return gen1(lists, {}, 0, []);
+	var lists = _.map(merges, function(elem) {
+		if (!_.isArray(elem))
+			elem = [elem];
+		return gen2(elem);
+	});
+	console.log("genMerge lists:", lists);
+	var result = gen1(lists, {}, 0, []);
+	console.log("genMerge result:", result);
+	return result;
 }
 
 function genList(spec) {
+	console.log("genList:", spec);
 	assert(_.isArray(spec));
 	var lists = _.map(spec, function(elem) { return gen2(elem); });
 	return combineLists(lists);
@@ -100,6 +103,8 @@ function combineLists(elem) {
 	var list = [];
 	if (_.isArray(elem) && !_.isEmpty(elem)) {
 		list = elem[0];
+		if (!_.isArray(list))
+			list = [list];
 		for (var i = 1; i < elem.length; i++) {
 			console.log("list@"+i, list);
 			list = _(list).map(function(x) {
@@ -113,28 +118,7 @@ function combineLists(elem) {
 	return list;
 }
 
-function genMergex(mergeSpec, obj0, index, acc) {
-	console.log("genMerge", mergeSpec, obj0, index, acc);
-	assert(_.isArray(mergeSpec));
-	if (index >= mergeSpec.length) {
-		acc.push(obj0);
-		return acc;
-	}
-
-	var elem = mergeSpec[index];
-	console.log("elem:", elem);
-	if (_.isArray(elem)) {
-		for (var j = 0; j < elem.length; j++) {
-			var obj1 = gen2(elem, obj0, acc);
-			var obj1 = _.merge({}, obj0, elem[j]);
-			gen1(mergeSpec, obj1, index + 1, acc);
-		}
-	}
-
-	return acc;
-}
-
-console.log();
+/*console.log();
 console.log(1);
 console.log(JSON.stringify(gen1(spec1, {}, 0, []), null, '  '));
 
@@ -153,6 +137,7 @@ console.log(JSON.stringify(gen2(spec4), null, '  '));
 console.log();
 console.log(5);
 console.log(JSON.stringify(gen2(spec5), null, '  '));
+*/
 
 console.log();
 console.log(6);
