33f0a7d5dd0b4acc01306945c0d42e321957bc66
Ellis Whitehead <ellis@ellisw.net>
2015-09-21 11:20:35 +0200

    minor

--- roboliq/runall.sh
+++ roboliq/runall.sh
@@ -1,4 +1,4 @@
 for file in protocols/protocol*.json; do
   rm -f protocols/output/$(basename $file .json).out.json
-  node test.js -O protocols/output $file
+  node src/test.js -O protocols/output $file
 done
