8ff085f937c47e95a4f69fc937f98c1b944476eb
Ellis Whitehead <ellis@ellisw.net>
2015-09-23 14:58:59 +0200

    documentation for commands/equipment

--- roboliq/src/commands/equipment.js
+++ roboliq/src/commands/equipment.js
@@ -1,20 +1,20 @@
+/**
+ * Namespace for the ``equipment`` commands.
+ * @namespace equipment
+ * @version v1
+ */
+
+/**
+ * Equipment commands module.
+ * @module commands/equipment
+ * @return {Protocol}
+ * @version v1
+ */
 var _ = require('lodash');
 var jmespath = require('jmespath');
 var commandHelper = require('../commandHelper.js');
 var expect = require('../expect.js');
 var misc = require('../misc.js');
-
-var objectToPredicateConverters = {
-	"Centrifuge": function(name, object) {
-		return {
-			value: [{
-				"isCentrifuge": {
-					"equipment": name
-				}
-			}]
-		};
-	},
-};
 
 function closeAll(params, data, effects) {
 	expect.paramsRequired(params, ["equipment"]);
@@ -25,11 +25,42 @@ function closeAll(params, data, effects) {
 	_.forEach(equipmentData.sitesInternal, function(site) { effects[site+".closed"] = true; });
 }
 
+/**
+ * Handlers for {@link equipment} commands.
+ * @static
+ */
 var commandHandlers = {
+	/**
+	 * Run the given equipment.
+	 *
+	 * This is a generic command, and any addition parameters may be passed that
+	 * are required by the target equipment.
+	 *
+	 * @typedef _run
+	 * @memberof equipment
+	 * @property {string} command - "equipment._run"
+	 * @property {string} agent - Agent identifier
+	 * @property {string} equipment - Equipment identifier
+	 */
 	"equipment._run": function(params, data) {
 		expect.paramsRequired(params, ["agent", "equipment"]);
 		return {};
 	},
+	/**
+	 * Open the given equipment.
+	 *
+	 * This is a generic command that expands to a sub-command named
+	 * `equipment.open|${agent}|${equipment}`.
+	 * That command should be defined in your configuration for your lab.
+	 *
+	 * The handler should return effects indicating that the equipment is open.
+	 *
+	 * @typedef open
+	 * @memberof equipment
+	 * @property {string} command - "equipment.open"
+	 * @property {string} agent - Agent identifier
+	 * @property {string} equipment - Equipment identifier
+	 */
 	"equipment.open": function(params, data) {
 		var parsed = commandHelper.parseParams(params, data, {
 			agent: "name",
@@ -47,6 +78,24 @@ var commandHandlers = {
 			effects: _.zipObject([[params.equipment+".open", true]])
 		};
 	},
+	/**
+	 * Open an equipment site.
+	 * This command assumes that only one equipment site can be open at a time.
+	 *
+	 * This is a generic command that expands to a sub-command named
+	 * `equipment.openSite|${agent}|${equipment}`.
+	 * That command should be defined in your configuration for your lab.
+	 *
+	 * The handler should return effects indicating that the equipment is open,
+	 * the given site is open, and all other equipment sites are closed.
+	 *
+	 * @typedef openSite
+	 * @memberof equipment
+	 * @property {string} command - "equipment.openSite"
+	 * @property {string} agent - Agent identifier
+	 * @property {string} equipment - Equipment identifier
+	 * @property {string} site - Site identifier
+	 */
 	"equipment.openSite": function(params, data) {
 		var parsed = commandHelper.parseParams(params, data, {
 			agent: "name",
@@ -74,6 +123,22 @@ var commandHandlers = {
 			effects: effects
 		};
 	},
+	/**
+	 * Close the given equipment.
+	 *
+	 * This is a generic command that expands to a sub-command named
+	 * `equipment.close|${agent}|${equipment}`.
+	 * That command should be defined in your configuration for your lab.
+	 *
+	 * The handler should return effects indicating the the equipment is closed
+	 * and all of its sites are closed.
+	 *
+	 * @typedef close
+	 * @memberof equipment
+	 * @property {string} command - "equipment.close"
+	 * @property {string} agent - Agent identifier
+	 * @property {string} equipment - Equipment identifier
+	 */
 	"equipment.close": function(params, data) {
 		var parsed = commandHelper.parseParams(params, data, {
 			agent: "name",
@@ -100,6 +165,11 @@ var commandHandlers = {
 	},
 };
 
+/**
+ * Plan handler to allow other modules to use `equipment._close` and
+ * `equipment._openSite` as planning actions.
+ * @static
+ */
 var planHandlers = {
 	"equipment._close": function(params, parentParams, data) {
 		return [{
@@ -120,8 +190,6 @@ var planHandlers = {
 
 module.exports = {
 	roboliq: "v1",
-	//predicates: predicates,
-	objectToPredicateConverters: objectToPredicateConverters,
 	commandHandlers: commandHandlers,
 	planHandlers: planHandlers
 };
