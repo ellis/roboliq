3652d99cb5f3cfc39d61ec76da2d650ec893ae8f
Ellis Whitehead <ellis@ellisw.net>
2015-11-05 10:27:03 +0100

    troubleshooting pipetterTest

--- roboliq/src/commandHelper.js
+++ roboliq/src/commandHelper.js
@@ -208,7 +208,7 @@ function processValueTypeSingle(value0, type, data, name) {
 			if (data.commandSpecs.hasOwnProperty(type)) {
 				const spec = data.commandSpecs[type];
 				const parsed = parseParams(value0, data, spec.properties);
-				//console.log({type, parsed})
+				console.log({type, parsed})
 				return parsed;
 			}
 			else {
