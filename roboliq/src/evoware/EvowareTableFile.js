7504d46abec59a8eaabe812eb1225de75cda846b
Ellis Whitehead <ellis@ellisw.net>
2016-01-14 14:23:58 +0100

    EvowareTableFile.toString_carriers

--- roboliq/src/evoware/EvowareTableFile.js
+++ roboliq/src/evoware/EvowareTableFile.js
@@ -260,3 +260,135 @@ function parse14_getExternalCarrierGrids(externalObjects, lines) {
 		return [external.carrierName, gridIndex];
 	});
 }
+
+/*
+export function toStringWithLabware(carrierData, table) {
+		siteIdToLabel_m2 <- ResultC.map(siteToNameAndLabel_m) { case (cngsi, (label, _)) =>
+			for {
+				carrier <- ResultC.from(configFile.mapNameToCarrier.get(cngsi.carrierName), s"carrier `${cngsi.carrierName}` not present in the evoware config file")
+			} yield {
+				val siteId = CarrierGridSiteIndex(carrier.id, cngsi.gridIndex, cngsi.siteIndex - 1)
+				val label2 = label.split('.').last
+				siteId -> label2
+			}
+		}
+		siteIdToLabwareModel_m2 <- ResultC.map(siteToNameAndLabel_m) { case (cngsi, (_, labwareModelName)) =>
+			for {
+				carrier <- ResultC.from(configFile.mapNameToCarrier.get(cngsi.carrierName), s"carrier `${cngsi.carrierName}` not present in the evoware config file")
+				siteId = CarrierGridSiteIndex(carrier.id, cngsi.gridIndex, cngsi.siteIndex - 1)
+				labwareModel <- ResultC.from(configFile.mapNameToLabwareModel.get(labwareModelName), s"labware model `${labwareModelName}` not present in the evoware config file")
+			} yield siteId -> labwareModel
+		}
+	} yield {
+		val siteIdToLabel_m3 = siteIdToLabel_m ++ siteIdToLabel_m2
+		val siteIdToLabwareModel_m3 = siteIdToLabwareModel_m ++ siteIdToLabwareModel_m2
+		//println("siteIdToLabwareModel_m:")
+		//siteIdToLabwareModel_m3.foreach(println)
+		// TODO: output current date and time
+		// TODO: See whether we need to save the RES section when loading in the table
+		// TODO: do we need to save values for the 999 line when loading the table?
+		val l = List(
+				"00000000",
+				"20000101_000000 No log in       ",
+				"                                                                                                                                ",
+				"No user logged in                                                                                                               ",
+				"--{ RES }--",
+				"V;200",
+				"--{ CFG }--",
+				"999;219;32;"
+			) ++
+			toString_carriers(carrierData, table) ++
+			toString_tableLabware(siteIdToLabel_m3, siteIdToLabwareModel_m3) ++
+			toString_hotels() ++
+			toString_externals() ++
+			toString_externalLabware(siteIdToLabwareModel_m3) ++
+			toString_externalGrids() ++
+			List("996;0;0;", "--{ RPG }--")
+		l.mkString("\n")
+	}
+}
+*/
+
+export function toString_carriers(carrierData, table) {
+	// Get list [[gridIndex, carrier.id]] for internal sites
+	// [[a, b]]
+	const gridToCarrierId_l = _(table).map((c, carrierName) => {
+		console.log({c, carrierName})
+		const carrier = carrierData.nameToCarrier[carrierName];
+		return _.map(c, (x, gridIndexText) => {
+			if (x.internal) {
+				try {
+					const gridIndex = parseInt(gridIndexText);
+					return [gridIndex, carrier.id];
+				} catch(e) {
+					// Do nothing
+				}
+			}
+			return undefined;
+		});
+	}).flatten().compact().value();
+	const gridToCarrierId_m = _.zipObject(gridToCarrierId_l);
+	console.log({gridToCarrierId_l, gridToCarrierId_m})
+
+	const l = _.times(99, gridIndex => _.get(gridToCarrierId_m, gridIndex, -1));
+
+	return `14;${l.join(";")};`;
+}
+
+/*
+private def toString_tableLabware(
+	siteIdToLabel_m2: Map[CarrierGridSiteIndex, String],
+	siteIdToLabwareModel_m2: Map[CarrierGridSiteIndex, EvowareLabwareModel]
+): List[String] = {
+	carrierIdInternal_l.toList.zipWithIndex.flatMap {
+		case (None, _) => List("998;0;")
+		case (Some(carrierId), gridIndex) =>
+			val carrier = configFile.mapIdToCarrier(carrierId)
+			//val sSiteCount = if (carrier.nSites > 0) carrier.nSites.toString else ""
+			List(
+				"998;"+carrier.nSites+";"+((0 until carrier.nSites).map(siteIndex => {
+					val siteId = CarrierGridSiteIndex(carrierId, gridIndex, siteIndex)
+					siteIdToLabwareModel_m2.get(siteId) match {
+						case None => ""
+						case Some(labwareModel) => labwareModel.sName
+					}
+				}).mkString(";"))+";",
+				"998;"+((0 until carrier.nSites).map(siteIndex => {
+					val siteId = CarrierGridSiteIndex(carrierId, gridIndex, siteIndex)
+					siteIdToLabel_m2.get(siteId) match {
+						case None => ""
+						case Some(sLabel) => sLabel
+					}
+				}).mkString(";"))+";"
+			)
+	}
+}
+
+private def toString_hotels(): List[String] = {
+	("998;"+hotelObject_l.length+";") :: hotelObject_l.map(o => "998;"+o.parent.id+";"+o.gridIndex+";")
+}
+
+private def toString_externals(): List[String] = {
+	("998;"+externalObject_l.length+";") :: externalObject_l.map(o => "998;"+o.n1+";"+o.n2+";"+o.carrier.sName+";")
+}
+
+private def toString_externalLabware(
+	siteIdToLabwareModel_m2: Map[CarrierGridSiteIndex, EvowareLabwareModel]
+): List[String] = {
+	// List of external carriers
+	val carrierId_l: Set[Int] = externalObject_l.map(_.carrier.id).toSet
+	val siteIdToLabwareModel_m3 = siteIdToLabwareModel_m2.filterKeys(siteId => carrierId_l.contains(siteId.carrierId))
+	// Map of external carrier to labware model
+	val carrierIdToLabwareModel_m: Map[Int, EvowareLabwareModel]
+		= siteIdToLabwareModel_m3.toList.groupBy(_._1.carrierId).mapValues(l => l.head._2)
+	// List of external carriers with labware on them
+	val lCarrierToLabware = carrierIdToLabwareModel_m.toList.map { case (carrierId, labwareModelE) =>
+		s"998;${carrierId};${labwareModelE.sName};"
+	}
+	("998;"+carrierIdToLabwareModel_m.size+";") :: lCarrierToLabware
+}
+
+private def toString_externalGrids(): List[String] = {
+	externalObject_l.map(o => "998;"+carrierIdToGrids_m(o.carrier.id).head+";")
+}
+*/
--- roboliq/tests/EvowareTableFileTest.js
+++ roboliq/tests/EvowareTableFileTest.js
@@ -8,9 +8,36 @@ describe('EvowareTableFile', function() {
 		it('should load table from NewLayout_Feb2015.ewt', function () {
 			const carrierData = EvowareCarrierFile.loadEvowareCarrierData("../testdata/bsse-mario/Carrier.cfg");
 			const tableData = EvowareTableFile.load(carrierData, "../testdata/bsse-mario/NewLayout_Feb2015.ewt");
-			console.log(JSON.stringify(tableData, null, '\t'));
+			//console.log(JSON.stringify(tableData, null, '\t'));
 			//result.printCarriersById();
-			should.deepEqual(_.isEmpty(result), false);
+			//should.deepEqual(_.isEmpty(result), false);
+		});
+	});
+
+	describe('toString_carriers', function() {
+		it("should stringify a table with system liquid and wash station", function() {
+			const carrierData = EvowareCarrierFile.loadEvowareCarrierData("../testdata/bsse-mario/Carrier.cfg");
+			/*const carrierData = {
+				nameToCarrier
+			}*/
+			const table = {
+				"System": {
+					"-1": {
+						"external": { "n1": 4, "n2": 0 }
+					}
+				},
+				"Wash Station Clean": {
+					"1": {
+						"0": { "labwareModelName": "Wash Station Cleaner shallow" },
+						"1": { "labwareModelName": "Wash Station Waste" },
+						"2": { "labwareModelName": "Wash Station Cleaner deep" },
+						"internal": true
+					}
+				},
+			};
+
+			const s = EvowareTableFile.toString_carriers(carrierData, table);
+			should.deepEqual(s, "14;-1;239;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;-1;");
 		});
 	});
 });
