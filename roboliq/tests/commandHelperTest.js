632f7e48ebc153d7a9fc056447e9baac425b2d44
Ellis Whitehead <ellis@ellisw.net>
2016-02-06 11:34:41 +0100

    add another test for commandHelper.stepify that tests arrays of arrays

--- roboliq/tests/commandHelperTest.js
+++ roboliq/tests/commandHelperTest.js
@@ -458,5 +458,17 @@ describe('commandHelper', function() {
 				}
 			);
 		});
+		it('should turn array of arrays into object', () => {
+			should.deepEqual(
+				commandHelper.stepify([[], [{n: 1}, {n: 2}], [], [[{n: 3}, {n: 4}, {n: 5}]]]),
+				{
+					1: {n: 1},
+					2: {n: 2},
+					3: {n: 3},
+					4: {n: 4},
+					5: {n: 5}
+				}
+			);
+		});
 	});
 });
