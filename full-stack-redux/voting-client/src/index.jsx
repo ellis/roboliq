c7680230c03ef3d995734de2fccccf38b11cf929
Ellis Whitehead <ellis@ellisw.net>
2016-02-15 13:16:41 +0100

    works with react-router 1.0.3

--- full-stack-redux/voting-client/src/index.jsx
+++ full-stack-redux/voting-client/src/index.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {IndexRoute, Route, Router, browserHistory} from 'react-router';
+//import {IndexRoute, Route, Router, browserHistory} from 'react-router';
+import {Route, Router} from 'react-router';
 import App from './components/App';
 import Results from './components/Results';
 import Voting from './components/Voting';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
+/*
 const router = (
-	<Router history={browserHistory}>
+	<Router>
 		<Route path="/" component={App}>
 			<IndexRoute component={Results}/>
 			<Route path="voting" component={Voting}/>
+			// <Route path="results" component={Results}/>
+		</Route>
+	</Router>
+);
+*/
+const router = (
+	<Router>
+		<Route component={App}>
+			<Route path="/results" component={Results}/>
+			<Route path="/" component={Voting}/>
 			// <Route path="results" component={Results}/>
 		</Route>
 	</Router>
