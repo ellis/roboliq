1f4b70dfce3383baa9f32b7cda483c290325c3e5
Ellis Whitehead <ellis.whitehead@bsse.ethz.ch>
2016-03-07 11:58:06 +0100

    mario: add HOTEL4 sites

--- roboliq/src/config/ourlab.js
+++ roboliq/src/config/ourlab.js
@@ -167,6 +167,10 @@ module.exports = {
 					"CENTRIFUGE_2": { "type": "Site", "evowareCarrier": "Centrifuge", "evowareGrid": 54, "evowareSite": 2, "closed": true },
 					"CENTRIFUGE_3": { "type": "Site", "evowareCarrier": "Centrifuge", "evowareGrid": 54, "evowareSite": 1, "closed": true },
 					"CENTRIFUGE_4": { "type": "Site", "evowareCarrier": "Centrifuge", "evowareGrid": 54, "evowareSite": 2, "closed": true },
+					"HOTEL4_1": { "type": "Site", "evowareCarrier": "Hotel 4Pos Transfer Grid 69", "evowareGrid": 69, "evowareSite": 1 },
+					"HOTEL4_2": { "type": "Site", "evowareCarrier": "Hotel 4Pos Transfer Grid 69", "evowareGrid": 69, "evowareSite": 2 },
+					"HOTEL4_3": { "type": "Site", "evowareCarrier": "Hotel 4Pos Transfer Grid 69", "evowareGrid": 69, "evowareSite": 3 },
+					"HOTEL4_4": { "type": "Site", "evowareCarrier": "Hotel 4Pos Transfer Grid 69", "evowareGrid": 69, "evowareSite": 4 },
 					"HOTEL32_A1": { "type": "Site", "evowareCarrier": "Shelf 32Pos Microplate", "evowareGrid": 11, "evowareSite": 4 },
 					"HOTEL32_B1": { "type": "Site", "evowareCarrier": "Shelf 32Pos Microplate", "evowareGrid": 11, "evowareSite": 3 },
 					"HOTEL32_C1": { "type": "Site", "evowareCarrier": "Shelf 32Pos Microplate", "evowareGrid": 11, "evowareSite": 2 },
@@ -440,7 +444,7 @@ module.exports = {
 		// short-format plate sites (non-tall labware, not for deep well plates)
 		makeSiteModelPredicates({
 			siteModel: "ourlab.mario.siteModel_short",
-			sites: ["HOTEL32_A1", "HOTEL32_B1", "HOTEL32_C1", "HOTEL32_D1", "HOTEL32_A2", "HOTEL32_B2", "HOTEL32_C2", "HOTEL32_D2", "HOTEL32_A3", "HOTEL32_B3", "HOTEL32_C3", "HOTEL32_D3", "HOTEL32_A4", "HOTEL32_B4", "HOTEL32_C4", "HOTEL32_D4", "HOTEL32_A5", "HOTEL32_B5", "HOTEL32_C5", "HOTEL32_D5", "HOTEL32_A6", "HOTEL32_B6", "HOTEL32_C6", "HOTEL32_D6", "HOTEL32_A7", "HOTEL32_B7", "HOTEL32_C7", "HOTEL32_D7", "HOTEL32_A8", "HOTEL32_B8", "HOTEL32_C8", "HOTEL32_D8", "READER", "ROBOPEEL", "ROBOSEAL"].map(s => "ourlab.mario.site."+s),
+			sites: ["HOTEL4_1", "HOTEL4_2", "HOTEL4_3", "HOTEL4_4", "HOTEL32_A1", "HOTEL32_B1", "HOTEL32_C1", "HOTEL32_D1", "HOTEL32_A2", "HOTEL32_B2", "HOTEL32_C2", "HOTEL32_D2", "HOTEL32_A3", "HOTEL32_B3", "HOTEL32_C3", "HOTEL32_D3", "HOTEL32_A4", "HOTEL32_B4", "HOTEL32_C4", "HOTEL32_D4", "HOTEL32_A5", "HOTEL32_B5", "HOTEL32_C5", "HOTEL32_D5", "HOTEL32_A6", "HOTEL32_B6", "HOTEL32_C6", "HOTEL32_D6", "HOTEL32_A7", "HOTEL32_B7", "HOTEL32_C7", "HOTEL32_D7", "HOTEL32_A8", "HOTEL32_B8", "HOTEL32_C8", "HOTEL32_D8", "READER", "ROBOPEEL", "ROBOSEAL"].map(s => "ourlab.mario.site."+s),
 			labwareModels: ["ourlab.model.plateModel_96_square_transparent_nunc", "ourlab.model.plateModel_384_square"]
 		}),
 		// Bench sites that don't have any obstructions, so deep well plates can fit on them too
@@ -463,6 +467,7 @@ module.exports = {
 				Narrow: [
 					[
 						"P1", "P2", "P3", "P6", "P7", "P8", "ROBOPEEL", "ROBOSEAL", "REGRIP",
+						"HOTEL4_1", "HOTEL4_2", "HOTEL4_3", "HOTEL4_4",
 						"HOTEL32_A1", "HOTEL32_B1", "HOTEL32_C1", "HOTEL32_D1",
 						"HOTEL32_A2", "HOTEL32_B2", "HOTEL32_C2", "HOTEL32_D2",
 						"HOTEL32_A3", "HOTEL32_B3", "HOTEL32_C3", "HOTEL32_D3",
