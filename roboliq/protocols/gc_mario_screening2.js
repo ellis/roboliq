a2a0dcaed5d2f1747d25cc2ee9502d7457e5d4de
Ellis Whitehead <ellis@ellisw.net>
2016-07-14 07:26:10 +0200

    protocols

--- roboliq/protocols/gc_mario_screening2.js
+++ roboliq/protocols/gc_mario_screening2.js
@@ -1,21 +1,23 @@
-const fractional_5_1 = {
+import _ from 'lodash';
+
+const fractional_5_1a = {
 	x1: [-1,  1, -1,  1, -1,  1, -1,  1, -1,  1, -1,  1, -1,  1, -1,  1],
 	x2: [-1, -1,  1,  1, -1, -1,  1,  1, -1, -1,  1,  1, -1, -1,  1,  1],
 	x3: [-1, -1, -1, -1,  1,  1,  1,  1, -1, -1, -1, -1,  1,  1,  1,  1],
 	x4: [-1, -1, -1, -1, -1, -1, -1, -1,  1,  1,  1,  1,  1,  1,  1,  1],
 	x5: [ 1, -1, -1,  1, -1,  1,  1, -1, -1,  1,  1, -1,  1, -1, -1,  1]
 };
+const fractional_5_1b = _.set(_.cloneDeep(fractional_5_1a), "x5", fractional_5_1a.x5.map(x => -1));
 
 function addAxialRuns(cols, alpha) {
 	const keys = _.keys(cols);
 	const n = keys.length;
-	const a = (alpha) ? alpha : sqrt(sqrt(n));
+	const a = (alpha) ? alpha : Math.sqrt(Math.sqrt(n));
 	for (let i = 0; i < n; i++) {
 		const key = keys[i];
-		const axialValues = Array(n * 2);
-		_.fill(axialValues, 0);
-		axialValues[i*n] = -a;
-		axialValues[i*n + 1] = a;
+		const axialValues = _.fill(Array(n * 2), 0);
+		axialValues[i*2] = -a;
+		axialValues[i*2 + 1] = a;
 		cols[key].push(...axialValues);
 	}
 }
@@ -26,3 +28,17 @@ function addCenterRuns(cols, n) {
 		l.push(...l0);
 	});
 }
+
+function createFractional_5_1(fractionIndex, axialAlpha, centerCount) {
+	const fractional = (fractionIndex === 1) ? fractional_5_1a : fractional_5_1b;
+	const l = _.cloneDeep(fractional);
+	addAxialRuns(l, axialAlpha);
+	addCenterRuns(l, centerCount);
+	const rows = _.values(_.merge({}, ... _.map(l, (values, key) => values.map(x => ({[key]: x})))));
+	return rows;
+}
+
+const rows1 = createFractional_5_1(1, undefined, 6)
+console.log(rows1);
+const rows2 = createFractional_5_1(2, undefined, 6)
+const rows3 = createFractional_5_1(1, undefined, 6)
