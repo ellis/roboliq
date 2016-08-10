7972b0d87aefc57073b50eec203b9e4b16fc181e
Ellis Whitehead <ellis@ellisw.net>
2016-08-10 10:39:28 +0200

    gc_mario_screening4

--- roboliq/protocols/gc_mario_screening4.js
+++ roboliq/protocols/gc_mario_screening4.js
@@ -60,10 +60,10 @@ const rows2 = [
 const sources = {
 	buffer: 40,
 	strain: 20,
-	glucose: 10,
+	glucose: 20,
 	nitrogen1: 40,
 	nitrogen2: 40,
-	trace1: 20,
+	trace1: 10,
 	trace2: 40
 };
 
@@ -141,7 +141,7 @@ function makeDesign(group, initialRows, centers, deltas) {
 			"bufferV=calculate": {expression: `to(fullVolume * (${centers.buffer}) / (${sources.buffer}), "ul")`, decimals: 1},
 			"strainV=calculate": {expression: `to(fullVolume * (${centers.strain}) / (${sources.strain}), "ul")`, decimals: 1},
 			"glucoseV=calculate": {expression: `to(fullVolume * ((${centers.glucose}) + x1 * (${deltas.glucose})) / (${sources.glucose}), "ul")`, decimals: 1},
-			"nitrogen1V=calculate": {expression: `max(0 ul, to(fullVolume * ((${centers.nitrogen1}) + x2 * (${deltas.nitrogen1})) / (${sources.nitrogen1}), "ul"))`, decimals: 1},
+			"nitrogen1V=calculate": {expression: `max(4 ul, to(fullVolume * ((${centers.nitrogen1}) + x2 * (${deltas.nitrogen1})) / (${sources.nitrogen1}), "ul"))`, decimals: 1},
 			"nitrogen2V=calculate": {expression: `max(0 ul, to(fullVolume * ((${centers.nitrogen2}) + x3 * (${deltas.nitrogen2})) / (${sources.nitrogen2}), "ul"))`, decimals: 1},
 			"trace1V=calculate": {expression: `max(0 ul, to(fullVolume * ((${centers.trace1}) + x4 * (${deltas.trace1})) / (${sources.trace1}), "ul"))`, decimals: 1},
 			// Trace 2 has to be pipetted with large tips, so make sure it has at least 3ul
@@ -149,7 +149,7 @@ function makeDesign(group, initialRows, centers, deltas) {
 			".tooSmall=case": {
 				cases: [
 					{where: 'glucoseV < (4ul)', conditions: {glucoseV: "0 ul"}},
-					{where: 'nitrogen1V < (4ul)', conditions: {nitrogen1V: "0 ul"}},
+					//{where: 'nitrogen1V < (4ul)', conditions: {nitrogen1V: "0 ul"}},
 					{where: 'nitrogen2V < (4ul)', conditions: {nitrogen2V: "0 ul"}},
 					{where: 'trace1V < (4ul)', conditions: {trace1V: "0 ul"}},
 					{where: 'trace1V < (4ul)', conditions: {trace1V: "0 ul"}},
