cd4acbff545fd2efebfd20f6d25d132702a53b1e
Oskari Vinko <vinkoo@student.ethz.ch>
2017-03-29 18:00:33 +0200

    Changed shell script protocol, yeast transformation plos fixes.

--- yeast-transformation-1.sh
+++ yeast-transformation-1.sh
@@ -1,4 +1,4 @@
-# 
+#
 # npm run processor -- --progress -P 'C:\ProgramData\Tecan\EVOware\database\scripts\OV' --evoware testdata/bsse-mario/Carrier.cfg,testdata/bsse-mario/OV_TranformationTestNew_20170111.ewt,ourlab.mario.evoware config/bsse-mario.js protocols/yeast-transformation-cells.yaml
 #
 # npm run processor -- --progress -P 'C:\ProgramData\Tecan\EVOware\database\scripts\OV' --evoware testdata/bsse-mario/Carrier.cfg,testdata/bsse-mario/OV_TranformationTestNew_20170111.ewt,ourlab.mario.evoware config/bsse-mario.js protocols/yeast-transformation-dmso.yaml
@@ -15,4 +15,6 @@
 #
 # npm run processor -- --progress -P 'C:\ProgramData\Tecan\EVOware\database\scripts\OV' --evoware testdata/bsse-mario/Carrier.cfg,testdata/bsse-mario/OV_TranformationTestNew_20170111.ewt,ourlab.mario.evoware config/bsse-mario.js protocols/yeast-transformation-waterresusp.yaml
 
-npm run processor -- --progress -P 'C:\ProgramData\Tecan\EVOware\database\scripts\OV' --evoware testdata/bsse-mario/Carrier.cfg,testdata/bsse-mario/OV_TranformationTestNew_20170111.ewt,ourlab.mario.evoware config/bsse-mario.js protocols/yeast-transformation-complete.yaml
+# npm run processor -- --progress -P 'C:\ProgramData\Tecan\EVOware\database\scripts\OV' --evoware testdata/bsse-mario/Carrier.cfg,testdata/bsse-mario/OV_TranformationTestNew_20170111.ewt,ourlab.mario.evoware config/bsse-mario.js protocols/yeast-transformation-complete.yaml
+
+npm run processor -- --progress -P 'C:\ProgramData\Tecan\EVOware\database\scripts\OV' --evoware testdata/bsse-mario/Carrier.cfg,testdata/bsse-mario/OV_TranformationTestNew_20170111.ewt,ourlab.mario.evoware config/bsse-mario.js protocols/yeast-transformation-plos.yaml
